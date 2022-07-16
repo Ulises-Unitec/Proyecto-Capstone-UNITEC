ac443cfee87d0acb6c9b67d3eedb93da11321bf1
“Ulises-Unitec” <96402455+Ulises-Unitec@users.noreply.github.com>
2022-07-15 20:09:54 -0500

    Create facecheck.py

---
+++ python/facedetection/facecheck.py
@@ -0,0 +1,10 @@
+from deepface import DeepFace
+
+models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
+metrics = ["cosine", "euclidean", "euclidean_l2"]
+
+df = DeepFace.find(img_path = "people/JAZV.jpg",db_path= "my_db/", enforce_detection="true")
+
+
+print ("Resultado ")
+print (df)
