5613b83a423f9289975bc25f31fdd836eff88130
“Ulises-Unitec” <96402455+Ulises-Unitec@users.noreply.github.com>
2022-07-29 19:58:22 -0500

    Update facecheck.py

--- python/facedetection/facecheck.py
+++ python/facedetection/facecheck.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
 metrics = ["cosine", "euclidean", "euclidean_l2"]
 
-df = DeepFace.find(img_path = "people/YAR.jpg",db_path= "my_db/", enforce_detection="true")
+df = DeepFace.find(img_path = "people/MGMT.jpg",db_path= "my_db/", enforce_detection="true")
 
 
 print ("Resultado ")
