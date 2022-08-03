2c3206baf8d33bcb8f2b08b670029b6ac7486384
“Ulises-Unitec” <96402455+Ulises-Unitec@users.noreply.github.com>
2022-08-03 13:42:05 -0500

    Update facecheck.py

--- python/facedetection/facecheck.py
+++ python/facedetection/facecheck.py
@@ -4,12 +4,8 @@ models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID",
 metrics = ["cosine", "euclidean", "euclidean_l2"]
 
 df = DeepFace.find(img_path = "alumnos/ASH.jpg",db_path= "my_db/", enforce_detection="true")
-df = DeepFace.find(img_path = "alumnos/PAZ.jpg",db_path= "my_db/", enforce_detection="true")
-df = DeepFace.find(img_path = "alumnos/YAR.jpg",db_path= "my_db/", enforce_detection="true")
-df = DeepFace.find(img_path = "alumnos/LCPG.jpg",db_path= "my_db/", enforce_detection="true")
-df = DeepFace.find(img_path = "alumnos/AIOC.jpg",db_path= "my_db/", enforce_detection="true")
 
 df = DeepFace.find(img_path = "profesores/MGMT.jpg",db_path= "my_db/", enforce_detection="true")
-df = DeepFace.find(img_path = "profesores/BRNL.jpg",db_path= "my_db/", enforce_detection="true")
+
 print ("Resultado ")
 print (df)
